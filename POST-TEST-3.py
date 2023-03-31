17bea56a335897de8559a573ab2cf155e935c576
mhnbl <125839542+mhnbl@users.noreply.github.com>
2023-03-31 21:38:35 +0800

    Update POST-TEST-3.py

--- POST-TEST-3.py
+++ POST-TEST-3.py
@@ -1,52 +1,49 @@
 import os
 import time
-import json
 from prettytable import PrettyTable
 os.system('cls')
 
-with open('log.json') as file:
-    data_login = json.load(file)
-
 tabel = PrettyTable()
-tabel.field_names = ["No.","Jam","Tujuan","Nomor Kereta"]
-class linkedList:
-    class Node:
-        def __init__(self,jam,tujuan,nomor, nextNode = None, prevNode = None):
-            self.next = nextNode
-            self.prev = prevNode
-            self.jam = jam
-            self.tujuan = tujuan
-            self.nomor = nomor
+tabel.field_names = ["No.","Jam","Tujuan","Nomor Kereta","Penumpang"]
 
+class Node:
+    def __init__(self,jam,tujuan,nomor,penumpang, nextNode = None, prevNode = None):
+        self.next = nextNode
+        self.prev = prevNode
+        self.jam = jam
+        self.tujuan = tujuan
+        self.nomor = nomor
+        self.penumpang = penumpang
+
+class linkedList:
     def __init__(self):
         self.head = None
         self.history = []
     
     def length(self):
-        pointer = self.head
         length = 0
-        while pointer is not None:
+        while self.head is not None:
             length += 1
-            pointer = pointer.next
+            pointer = self.head.next
         return length
 
     def view(self):
         if self.head == None:
-            print("\nJADWAL KOSONG\n")
+            print("\n    JADWAL KOSONG\n\n")
             Q()
         else:
             kolom = mylist.head
             tabel.clear_rows()
             num = 1
             while kolom:
-                tabel.add_row([num,kolom.jam,kolom.tujuan,kolom.nomor])
+                tabel.add_row([num,kolom.jam,kolom.tujuan,kolom.nomor,kolom.penumpang])
                 kolom = kolom.next
                 num += 1
             print(tabel.get_string())
 
     def viewHistory(self):
         if self.history == []:
-            print("\nHISTORI KOSONG\n")
+            print("\n    HISTORI KOSONG\n\n")
         else:
             h = self.head
             histori = PrettyTable()
@@ -56,9 +53,9 @@ class linkedList:
             histori.align = "l"
             print(histori)
 
-    def addFirst(self, jam,tujuan,nomor):
-        self.history.append(f'Menambah | {jam} | {tujuan} | {nomor} | di awal list')
-        newNode = self.Node(jam,tujuan,nomor)
+    def addFirst(self, jam,tujuan,nomor,penumpang):
+        self.history.append(f'Menambah | {jam} | {tujuan} | {nomor} | {penumpang} | di awal list')
+        newNode = Node(jam,tujuan,nomor,penumpang)
         if self.head == None:
             self.head = newNode
         else:
@@ -66,11 +63,11 @@ class linkedList:
             self.head.prev = newNode
             self.head = newNode
 
-    def addEnd(self,jam,tujuan,nomor):
-        self.history.append(f'Menambah | {jam} | {tujuan} | {nomor} | di akhir list')
-        newNode = self.Node(jam,tujuan,nomor)
+    def addEnd(self,jam,tujuan,nomor,penumpang):
+        self.history.append(f'Menambah | {jam} | {tujuan} | {nomor} | {penumpang} | di akhir list')
+        newNode = Node(jam,tujuan,nomor,penumpang)
         if self.head == None:
-            self.addFirst(jam,tujuan,nomor)
+            self.addFirst(jam,tujuan,nomor,penumpang)
         else:
             pointer = self.head
             while pointer.next != None:
@@ -78,52 +75,37 @@ class linkedList:
             pointer.next = newNode
             newNode.prev = pointer
 
-    def UpdateJam(self,position,newValue):
+    def Update(self,position,newValue,elemen):
         pointer = self.head
-        for i in range(1, position):
+        if position >= 1:
+            for i in range(1, position):
+                if pointer is None:
+                    break
+                pointer = pointer.next
             if pointer is None:
-                break
-            pointer = pointer.next
-        if pointer is None:
+                print(f'Nomor urut {position} tidak ditemukan. Silahkan coba lagi')
+                return 0
+            if elemen == 1:
+                pointer.jam = newValue
+                self.history.append(f'Mengubah nomor kereta | {pointer.jam} | menjadi | {newValue} |')
+            elif elemen == 2:
+                pointer.tujuan = newValue
+                self.history.append(f'Mengubah nomor kereta | {pointer.tujuan} | menjadi | {newValue} |')
+            elif elemen == 3:
+                pointer.nomor = newValue
+                self.history.append(f'Mengubah nomor kereta | {pointer.nomor} | menjadi | {newValue} |')
+            elif elemen == 4:
+                pointer.penumpang = newValue
+                self.history.append(f'Mengubah nomor kereta | {pointer.penumpang} | menjadi | {newValue} |')
+            print("Jadwal telah diperbarui")
+            return 1
+        else:
             print(f'Nomor urut {position} tidak ditemukan. Silahkan coba lagi')
             return 0
-        self.history.append(f'Mengubah jam | {pointer.jam} | menjadi | {newValue} |')
-        pointer.jam = newValue
-        print("Jadwal telah diperbarui")
-        return 1
-
-    def UpdateTujuan(self,position,newValue):
-        pointer = self.head
-        for i in range(1, position):
-            if pointer is None:
-                break
-            pointer = pointer.next
-        if pointer is None:
-            print(f'Nomor urut {position} tidak ditemukan. Silahkan coba lagi')
-            return 0
-        self.history.append(f'Mengubah tujuan | {pointer.tujuan} | menjadi | {newValue} |')
-        pointer.tujuan = newValue
-        print("Jadwal telah diperbarui")
-        return 1
-
-    def UpdateNomor(self,position,newValue):
-        pointer = self.head
-        for i in range(1, position):
-            if pointer is None:
-                break
-            pointer = pointer.next
-        if pointer is None:
-            print(f'Nomor urut {position} tidak ditemukan. Silahkan coba lagi')
-            return 0
-        self.history.append(f'Mengubah nomor kereta | {pointer.nomor} | menjadi | {newValue} |')
-        pointer.nomor = newValue
-        print("Jadwal telah diperbarui")
-        return 1
-        
-
+    
     def delete(self, position):
         if self.head is None:
-            print('\n JADWAL KOSONG \n')
+            print("\n    JADWAL KOSONG\n\n")
         temp = self.head
         if position == 1:
             self.head = temp.next
@@ -131,12 +113,10 @@ class linkedList:
                 self.head.prev = None
             temp = None
             return 1 
- 
         for i in range(1, position):
             if temp is None:
                 break
             temp = temp.next
-
         self.history.append(f'Mengahapus | {temp.jam} | {temp.tujuan} | {temp.nomor} | dari tabel')
         print(f'Menghapus | {temp.jam} | {temp.tujuan} | {temp.nomor} | \n')
         if temp is None:
@@ -146,21 +126,66 @@ class linkedList:
         if temp.prev is not None:
             temp.prev.next = temp.next
         temp = None
+        time.sleep(0.5)
+        return 1
     
-        time.sleep(1)
-        return 1         
+    def mergeSort(self, head,elemen):
+        if not head or not head.next:
+            return head
+        mid = self.getMiddle(head)
+        next_to_mid = mid.next
+        mid.next = None
+        left = self.mergeSort(head,elemen)
+        right = self.mergeSort(next_to_mid,elemen)
+        sorted_lst = self.merge(left, right,elemen)
+        return sorted_lst
+        
+    def getMiddle(self, head):
+        if not head:
+            return head
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        
+    def merge(self, left, right,elemen):
+        if not left:
+            return right
+        if not right:
+            return left
+        if elemen ==1 :
+            x = left.jam
+            y = right.jam
+        elif elemen == 2:
+            x = left.tujuan
+            y = right.tujuan
+        elif elemen == 3:
+            x = left.nomor
+            y = right.nomor
+        elif elemen == 4:
+            x = left.penumpang
+            y = right.penumpang
+        if x <= y:
+            result = left
+            result.next = self.merge(left.next, right,elemen)
+        else:
+            result = right
+            result.next = self.merge(left, right.next,elemen)
+        return result
 
 mylist = linkedList()
-mylist.addFirst('08:00','JAKARTA','KA123')
-mylist.addEnd('09:30','SURABAYA','KA456')
-mylist.addEnd('10:45','YOGYAKARTA','KA789')
-mylist.addEnd('12:15','BANDUNG','KA321')
-mylist.addEnd('13:30','SOLO','KA654')
-mylist.addEnd('14:45','MALANG','KA987')
-mylist.addEnd('16:00','SURABAYA','KA135')
-mylist.addEnd('18:00','BANDUNG','KA468')
-mylist.addEnd('19:30','JAKARTA','KA246')
-mylist.addEnd('21:00','BANDUNG','KA753')
+mylist.addFirst('18:00','JAKARTA','KA123',221)
+mylist.addEnd('09:30','SURABAYA','KA456',142)
+mylist.addEnd('14:45','YOGYAKARTA','KA789',52)
+mylist.addEnd('12:15','BANDUNG','KA321',341)
+mylist.addEnd('18:30','SOLO','KA654',122)
+mylist.addEnd('10:45','MALANG','KA987',194)
+mylist.addEnd('16:00','SURABAYA','KA135',123)
+mylist.addEnd('11:00','SEMARANG','KA468',342)
+mylist.addEnd('19:30','JAKARTA','KA246',129)
+mylist.addEnd('13:00','BANDUNG','KA753',172)
 
 def Q():
     tanya = input('\nKe menu utama? [y/t]: ').lower()
@@ -170,34 +195,79 @@ def Q():
     if tanya == 'y':
         main()
     elif tanya == 't':
-        print("\n    TERIMA KASIH\n\n")
+        x = 50*"-"
+        print(x,"TERIMA KASIH",x)
         exit()
 
 def main():
     os.system('cls')
-    print('\nJADWAL KEBERANGKATAN STASIUN TUGU')
-    print('[1] Tampilkan Jadwal')
-    print('[2] Tambahkan Data Di Awal')
-    print('[3] Tambahkan Data Di Akhir')
-    print('[4] Perbarui Data')
-    print('[5] Hapus data')
-    print('[6] Histori')
+    print('\n+-----------------------------------+')
+    print('| JADWAL KEBERANGKATAN STASIUN TUGU |')
+    print('+-----------------------------------+')
+    print('|     [1] Tampilkan Jadwal          |')
+    print('|     [2] Mengurutkan Jadwal        |')
+    print('|     [3] Tambahkan Data Di Awal    |')
+    print('|     [4] Tambahkan Data Di Akhir   |')
+    print('|     [5] Perbarui Data             |')
+    print('|     [6] Hapus data                |')
+    print('|     [7] Histori                   |')
+    print('+-----------------------------------+')
 
     ask = input('Pilih: ')
-    while ask not in ['1','2','3','4','5','6']:
+    while ask not in ['1','2','3','4','5','6','7']:
         print('Pilih dari pilihan yang tersedia')
         ask = input('Pilih: ')
         
     if ask == '1':
         print("Memproses...")
-        time.sleep(1)
+        time.sleep(0.5)
         os.system('cls')
         print('        JADWAL KEBERANGKATAN KERETA')
         mylist.view()
 
     elif ask == '2':
         print("Memproses...")
-        time.sleep(1)
+        time.sleep(0.5)
+        os.system('cls')
+        print('+---------------------------------------+')
+        print('|       MENGURUTKAN JADWAL KERETA       |')
+        print('+---------------------------------------+')
+        print('|    Jadwal Diurutkan berdasarkan :     |')
+        print('|    [1] Jam Keberangkatan              |')
+        print('|    [2] Tujuan Keberangkatan           |')
+        print('|    [3] Nomor Kereta                   |')
+        print('|    [4] Jumlah Penumpang               |')
+        print('+---------------------------------------+')
+        print
+        ask = input("Pilih: ")
+        while ask not in ['1','2','3','4']:
+            print("Pilih dari pilihan yang tersedia, mohon coba lagi")
+            ask = input(": ")
+        if ask == '1':
+            print('+-----------------------------------------------------+')
+            print('|             MENGURUTKAN BERDASARKAN JAM             |')
+            print('+-----------------------------------------------------+')
+            mylist.head = mylist.mergeSort(mylist.head,1)
+        if ask == '2':
+            print('+-----------------------------------------------------+')
+            print('|           MENGURUTKAN BERDASARKAN TUJUAN            |') 
+            print('+-----------------------------------------------------+')
+            mylist.head = mylist.mergeSort(mylist.head,2)
+        if ask == '3':
+            print('+-----------------------------------------------------+')
+            print('|         MENGURUTKAN BERDASARKAN NOMOR KERETA        |')
+            print('+-----------------------------------------------------+')
+            mylist.head = mylist.mergeSort(mylist.head,3)
+        if ask == '4': 
+            print('+-----------------------------------------------------+')
+            print('|          MENGURUTKAN BERDASARKAN PENUMPANG          |')
+            print('+-----------------------------------------------------+')
+            mylist.head = mylist.mergeSort(mylist.head,4)
+        mylist.view()
+
+    elif ask == '3':
+        print("Memproses...")
+        time.sleep(0.5)
         os.system('cls')
         print('      MENAMBAHKAN JADWAL KEBERANGKATAN')
         mylist.view()
@@ -205,13 +275,14 @@ def main():
         arv = input('Jam Keberangkatan\t: ')
         des = input('Tujuan\t\t\t: ').upper()
         num = input('Nomor Kereta\t\t: ').upper()
-        mylist.addFirst(arv,des,num)
+        sum = input('Jumlah Penumpang\t: ').upper()
+        mylist.addFirst(arv,des,num,sum)
         print('Data telah ditambahkan\n')
         mylist.view()
 
-    elif ask == '3':
+    elif ask == '4':
         print("Memproses...")
-        time.sleep(1)
+        time.sleep(0.5)
         os.system('cls')
         print('      MENAMBAHKAN JADWAL KEBERANGKATAN')
         mylist.view()
@@ -219,28 +290,32 @@ def main():
         arv = input('Jam Keberangkatan\t: ')
         des = input('Tujuan\t\t\t: ').upper()
         num = input('Nomor Kereta\t\t: ').upper()
-        mylist.addEnd(arv,des,num)
+        sum = input('Jumlah Penumpang\t: ').upper()
+        mylist.addFirst(arv,des,num,sum)
         print('Data telah ditambahkan\n')
         mylist.view()
 
-    elif ask == '4':
+    elif ask == '5':
         print("Memproses...")
-        time.sleep(1)
+        time.sleep(0.5)
         os.system('cls')
-        print('MEMPERBARUI JADWAL KEBERANGKATAN')
-        print('\nData yang ingin perbarui')
-        print('[1] Jam Keberangkatan')
-        print('[2] Tujuan Keberangkatan')
-        print('[3] Nomor Kereta')
+        print('+---------------------------------------+')
+        print('| MEMPERBARUI DATA JADWAL KEBERANGKATAN |')
+        print('+---------------------------------------+')
+        print('|      Data yang ingin perbarui :       |')
+        print('|    [1] Jam Keberangkatan              |')
+        print('|    [2] Tujuan Keberangkatan           |')
+        print('|    [3] Nomor Kereta                   |')
+        print('|    [4] Jumlah Penumpang               |')
+        print('+---------------------------------------+')
         
         tanya = input('Pilih: ')
-        while tanya not in ['1','2','3']:
+        while tanya not in ['1','2','3','4']:
             print('Pilih dari pilihan yang tersedia')
             tanya = input('Pilih: ')
-
         if tanya == '1':
             print("Memproses...")
-            time.sleep(1)
+            time.sleep(0.5)
             os.system('cls')
             print('        MEMPERBARUI JAM KEBERANGKATAN')
             mylist.view()
@@ -251,16 +326,15 @@ def main():
                         break
                     except ValueError:
                         print("Input harus berupa angka. Silakan coba lagi.")
-
                 new = input('Jam Baru\t: ').upper()
-                if mylist.UpdateJam(pos,new) == 1:
+                if mylist.Update(pos,new,1) == 1:
                     break
             print('')
             mylist.view()
 
         elif tanya == '2':
             print("Memproses...")
-            time.sleep(1)
+            time.sleep(0.5)
             os.system('cls')
             print('        MEMPERBARUI TUJUAN KERETA')
             mylist.view()
@@ -271,17 +345,15 @@ def main():
                         break
                     except ValueError:
                         print("Input harus berupa angka. Silakan coba lagi.")
-
                 new = input('Tujuan Baru\t: ').upper()
-                if mylist.UpdateTujuan(pos,new) == 1:
+                if mylist.Update(pos,new,2) == 1:
                     break
             print('')
             mylist.view()
 
-
         elif tanya == '3':
             print("Memproses...")
-            time.sleep(1)
+            time.sleep(0.5)
             os.system('cls')
             print('        MEMPERBARUI NOMOR KERETA')
             mylist.view()
@@ -293,14 +365,33 @@ def main():
                     except ValueError:
                         print("Input harus berupa angka. Silakan coba lagi.")
                 new = input('Nomor Baru\t: ').upper()
-                if mylist.UpdateNomor(pos,new) == 1:
+                if mylist.Update(pos,new,3) == 1:
                     break
             print('')
             mylist.view()
 
-    elif ask == '5':
+        elif tanya == '4':
+            print("Memproses...")
+            time.sleep(0.5)
+            os.system('cls')
+            print('        MEMPERBARUI JUMLAH PENUMPANG')
+            mylist.view()
+            while True:
+                while True:
+                    try:
+                        pos = int(input('\nNo. Urut\t: '))
+                        new = int(input('Jumlah Baru\t: '))
+                        break
+                    except ValueError:
+                        print("Input harus berupa angka. Silakan coba lagi.")
+                if mylist.Update(pos,new,4) == 1:
+                    break
+            print('')
+            mylist.view()
+
+    elif ask == '6':
         print("Memproses...")
-        time.sleep(1)
+        time.sleep(0.5)
         os.system('cls')
         print("       MENGHAPUS JADWAL KEBERANGKATAN")
         mylist.view()
@@ -318,65 +409,12 @@ def main():
             else:
                 print(f'Nomor urut {x} tidak ditemukan. Silahkan coba lagi')
                 
-    elif ask == '6':
+    elif ask == '7':
         print("Memproses...")
-        time.sleep(1)
+        time.sleep(0.5)
         os.system('cls')
         print("HISTORI PERUBAHAN TERHADAP JADWAL")
         mylist.viewHistory()
     Q()
 
-def aks():
-    tany = input("Kembali ke menu utama? [y/t]").lower()
-    while tany not in ["y","t"]:
-        print("Pilihan tidak tersedia, mohon coba lagi")
-        tany = input("Kembali ke menu utama? [y/t]").lower()
-    if tany == "y":
-        start()
-    else:
-        exit()
-
-def login():
-    usr = input('Masukkan username: ')
-    pin = input('Masukkan PIN: ')
-
-    if usr in data_login and data_login[usr]["pin"] == pin:
-        print("Log-In Berhasil")
-        print("Selamat Datang", data_login[usr]["nama"])
-        return 1
-    else:
-        print("Username dan Password salah, Mohon coba lagi")
-        aks()
-
-def addUsr():
-    name = input('\nNama anda: ')
-    username = input('Username baru: ')
-    pin = input('PIN baru: ')
-    data_login[username] = {"nama": name, "pin": pin}
-
-    with open('log.json', 'w') as file:
-        json.dump(data_login, file,indent = 2)
-
-    print('Akun', username, 'telah berhasil ditambahkan.')
-    return
-
-
-def start():
-    print("[1] Log-In")
-    print("[2] Buat Akun")
-    
-    ask = input("Pilih: ")
-    while ask not in ["1","2"]:
-        print("Pilihan tidak tersedia, mohon coba lagi")
-        ask = input("Pilih: ")
-
-    if ask == "1":
-        if login() == 1:
-            main()
-
-    if ask == "2":
-        addUsr()
-        aks()
-
-start()
-
+main()
